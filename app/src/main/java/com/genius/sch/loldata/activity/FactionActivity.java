5c4aa455870b07610273f1284fe9ea16d9cf7330
Admin <1205155699@qq.com>
2018-07-20 14:30:48 +0800

    英雄role添加第二个

---
+++ app/src/main/java/com/genius/sch/loldata/activity/FactionActivity.java
@@ -0,0 +1,20 @@
+package com.genius.sch.loldata.activity;
+
+import com.genius.sch.loldata.R;
+
+public class FactionActivity extends BaseActivity{
+    @Override
+    public int getActivity() {
+        return R.layout.activity_faction;
+    }
+
+    @Override
+    protected void initView() {
+        initTitle();
+    }
+
+    @Override
+    protected void main() {
+
+    }
+}
--- app/src/main/java/com/genius/sch/loldata/activity/MainActivity.java
+++ app/src/main/java/com/genius/sch/loldata/activity/MainActivity.java
@@ -1,5 +1,6 @@
 package com.genius.sch.loldata.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
@@ -99,8 +100,8 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
-            // Handle the camera action
+        if (id == R.id.nav_faction) {
+            startActivity(new Intent(this,FactionActivity.class));
         } else if (id == R.id.nav_gallery) {
 
         } else if (id == R.id.nav_slideshow) {
